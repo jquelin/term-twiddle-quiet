e8ed37be4ff52cee87ca90c45e938579ccb3d0bc
Jérôme Quelin <jquelin@gmail.com>
2010-01-09 18:57:12 +0100

    implement code (inspired by term::progressbar::quiet)

--- lib/Term/Twiddle/Quiet.pm
+++ lib/Term/Twiddle/Quiet.pm
@@ -5,5 +5,24 @@ use warnings;
 package Term::Twiddle::Quiet;
 # ABSTRACT: Twiddles a thingy while-u-wait if run interactively
 
+use IO::Interactive qw{ is_interactive };
+use Term::Twiddle;
+use Test::MockObject;
+
+sub new {
+    my $class = shift;
+
+    # interactive: return the real object
+    return Term::Twiddle->new(@_) if is_interactive;
+
+    # not interactive: let's mock it
+    my $mock = Test::MockObject->new;
+    $mock->set_true($_) for qw{
+        start stop thingy rate probability random stream
+        type width delay
+    };
+    return $mock;
+}
+
 1;
 __END__
