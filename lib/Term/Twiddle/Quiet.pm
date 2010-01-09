d503e17664a88d8aa7723689e4cb70e6230eb9fc
Jérôme Quelin <jquelin@gmail.com>
2010-01-09 19:03:36 +0100

    pod update

--- lib/Term/Twiddle/Quiet.pm
+++ lib/Term/Twiddle/Quiet.pm
@@ -9,6 +9,17 @@ use IO::Interactive qw{ is_interactive };
 use Term::Twiddle;
 use Test::MockObject;
 
+
+# -- public methods
+
+=method my $tw = Term::Twiddle::Quiet->new( \%params );
+
+Create and return twiddle. The twiddle will do nothing when activated if
+the program is ran non-interactively, otherwise it'll return a plain
+L<Term::Twiddle> object.
+
+=cut
+
 sub new {
     my $class = shift;
 
@@ -26,3 +37,26 @@ sub new {
 
 1;
 __END__
+
+=head1 SYNOPSIS
+
+    use Term::Twiddle::Quiet;
+    my $tw = Term::Twiddle::Quiet->new( \%params );
+    $tw->start;
+    # do some stuff taking a long time in here
+    $tw->stop;
+
+
+=head1 DESCRIPTION
+
+L<Term::Twiddle> is a nice module for showing spinning thingies on the
+terminal while waiting for an action to complete.
+
+L<Term::Twiddle::Quiet> acts very much like that module when it is run
+interactively. However, when it isn't run interactively (for example, as
+a cron job) then it does not show the twiddle.
+
+Other than this difference, it really act as a L<Term::Twiddle> with all
+its options, methods and restrictions (of course, it supports the same
+API) - cf its documentation for more information.
+
